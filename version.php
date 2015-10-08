36e0a555c4ff16ce2efff00364dde38f95fcbb67
callmngr <callmngr@e2ee9c56-5b25-498a-944d-c93643e4d956>
2015-10-08 08:09:04 +0000

    block/activity_list replace references to "quizport" by "taskchain", and references to "taskchain_navigation" to "activity_list"

--- version.php
+++ version.php
@@ -17,5 +17,5 @@
 $plugin->component = 'block_activity_list';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2015090903;
-$plugin->release   = '2015-09-09 (03)';
+$plugin->version   = 2015100804;
+$plugin->release   = '2015-10-08 (04)';
