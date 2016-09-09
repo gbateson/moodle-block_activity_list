d7f5728d651941580ed34d3544804639f9a9dbf6
callmngr <callmngr@e2ee9c56-5b25-498a-944d-c93643e4d956>
2016-09-09 06:51:18 +0000

    block_activity_list use standard moodleform in import/export scripts

--- export.php
+++ export.php
@@ -8,39 +8,57 @@
  * @license    you may not copy of distribute any part of this package without prior written permission
  */
 
+/** Include required files */
 require_once('../../config.php');
 require_once($CFG->dirroot.'/lib/filelib.php'); // send_file()
-require_once($CFG->dirroot.'/backup/backuplib.php'); // xml_tag_safe_content()
 
-$id = required_param('id', PARAM_INT); // block_instance id
+// cache the plugin name  - because it is quite long ;-)
+$plugin = 'block_activity_list';
 
-if (! $block_instance = get_record('block_instance', 'id', $id)) {
-    print_error('invalidinstanceid', 'block_activity_list');
+// get the incoming block_instance id
+$id = required_param('id', PARAM_INT);
+
+if (! $block_instance = $DB->get_record('block_instances', array('id' => $id))) {
+    print_error('invalidinstanceid', $plugin, '', $id);
 }
-
-if (! $block = get_record('block', 'id', $block_instance->blockid)) {
-    print_error('invalidblockid', 'block_activity_list', $block_instance->blockid);
+if (! $block = $DB->get_record('block', array('name' => $block_instance->blockname))) {
+    print_error('invalidblockname', $plugin, '', $block_instance);
 }
-
-if (! $course = get_record('course', 'id', $block_instance->pageid)) {
-    print_error('invalidcourseid', 'block_activity_list', $block_instance->pageid);
+if (! $context = $DB->get_record('context', array('id' => $block_instance->parentcontextid))) {
+    print_error('invalidcontextid', $plugin, '', $block_instance);
+}
+if (! $course = $DB->get_record('course', array('id' => $context->instanceid))) {
+    print_error('invalidcourseid', $plugin, '', $context);
 }
 
 require_login($course->id);
 
-$context = get_context_instance(CONTEXT_COURSE, $course->id);
+if (class_exists('context')) {
+    $context = context::instance_by_id($context->id);
+} else {
+    $context = get_context_instance_by_id($context->id);
+}
 require_capability('moodle/site:manageblocks', $context);
 
-$modinfo = get_fast_modinfo($COURSE, $USER->id);
+if (! isset($block->version)) {
+    $params = array('plugin' => 'block_maj_submissions', 'name' => 'version');
+    $block->version = $DB->get_field('config_plugins', 'value', $params);
+}
+
+$modinfo = get_fast_modinfo($course);
 
 $content = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 if ($config = unserialize(base64_decode($block_instance->configdata))) {
 
-    $content .= '<ACTIVITYLISTBLOCK>'."\n";
+    // set main XML tag name for this block's config settings
+    $BLOCK = strtoupper($block_instance->blockname);
+    $BLOCK = strtr($BLOCK, array('_' => '')).'BLOCK';
+
+    $content .= "<$BLOCK>\n";
     $content .= '  <VERSION>'.$block->version.'</VERSION>'."\n";
-    $content .= '  <POSITION>'.$block_instance->position.'</POSITION>'."\n";
-    $content .= '  <WEIGHT>'.$block_instance->weight.'</WEIGHT>'."\n";
-    $content .= '  <VISIBLE>'.$block_instance->visible.'</VISIBLE>'."\n";
+    $content .= '  <POSITION>'.$block_instance->defaultregion.'</POSITION>'."\n";
+    $content .= '  <WEIGHT>'.$block_instance->defaultweight.'</WEIGHT>'."\n";
+    $content .= '  <VISIBLE>1</VISIBLE>'."\n";
     $content .= '  <CONFIGFIELDS>'."\n";
 
     $config = get_object_vars($config);
@@ -48,31 +66,44 @@ if ($config = unserialize(base64_decode($block_instance->configdata))) {
         if (empty($name) || is_array($value) || is_object($value)) {
             continue; // shouldn't happen !!
         }
-        $content .= '    <CONFIGFIELD>'."\n";
+        $printedconfig = false;
         if (preg_match('/^cmids[0-9]+/', $name)) { // list of $cmids
-            $content .= '      <NAME>'.xml_tag_safe_content($name).'</NAME>'."\n";
+            $printedname = false;
             $value = explode(',', $value);
+            $value = array_filter($value);
             foreach ($value as $cmid) {
                 if (empty($modinfo->cms[$cmid])) {
                     continue; // shouldn't happen !!
                 }
+                if ($printedconfig==false) {
+                    $printedconfig = true;
+                    $content .= '    <CONFIGFIELD>'."\n";
+                }
+                if ($printedname==false) {
+                    $printedname = true; // first time only
+                    $content .= '      <NAME>'.xml_tag_safe_content($name).'</NAME>'."\n";
+                }
                 // for each $cmid we store the section number, activity type and activity name
                 // and hope that there is a matching activity in the import target course
                 $content .= '      <VALUE>'."\n";
-                $content .= '        <SECTIONNUM>'.xml_tag_safe_content($modinfo->cms[$cmid]->sectionnum).'</SECTIONNUM>'."\n";
-                $content .= '        <MODNAME>'.xml_tag_safe_content($modinfo->cms[$cmid]->modname).'</MODNAME>'."\n";
-                $content .= '        <NAME>'.xml_tag_safe_content($modinfo->cms[$cmid]->name).'</NAME>'."\n";
+                $content .= '        <SECTIONNUM>'.xml_tag_safe_content($modinfo->get_cm($cmid)->sectionnum).'</SECTIONNUM>'."\n";
+                $content .= '        <MODNAME>'.xml_tag_safe_content($modinfo->get_cm($cmid)->modname).'</MODNAME>'."\n";
+                $content .= '        <NAME>'.xml_tag_safe_content($modinfo->get_cm($cmid)->name).'</NAME>'."\n";
                 $content .= '      <VALUE>'."\n";
             }
         } else {
+            $printedconfig = true;
+            $content .= '    <CONFIGFIELD>'."\n";
             $content .= '      <NAME>'.xml_tag_safe_content($name).'</NAME>'."\n";
             $content .= '      <VALUE>'.xml_tag_safe_content($value).'</VALUE>'."\n";
         }
-        $content .= '    </CONFIGFIELD>'."\n";
+        if ($printedconfig==true) {
+            $content .= '    </CONFIGFIELD>'."\n";
+        }
     }
 
     $content .= '  </CONFIGFIELDS>'."\n";
-    $content .= '</ACTIVITYLISTBLOCK>'."\n";
+    $content .= "</$BLOCK>\n";
 }
 
 if (empty($config['title'])) {
@@ -81,3 +112,19 @@ if (empty($config['title'])) {
     $filename = clean_filename(strip_tags(format_string($config['title'], true)).'.xml');
 }
 send_file($content, $filename, 0, 0, true, true);
+
+/**
+ * xml_tag_safe_content
+ *
+ * copied from Moodle 1.9 backup/backuplib.php
+ */
+function xml_tag_safe_content($content) {
+    global $CFG;
+    //If enabled, we strip all the control chars (\x0-\x1f) from the text but tabs (\x9),
+    //newlines (\xa) and returns (\xd). The delete control char (\x7f) is also included.
+    //because they are forbiden in XML 1.0 specs. The expression below seems to be
+    //UTF-8 safe too because it simply ignores the rest of characters.
+    $content = preg_replace("/[\x-\x8\xb-\xc\xe-\x1f\x7f]/is","",$content);
+    $content = preg_replace("/\r\n|\r/", "\n", htmlspecialchars($content));
+    return $content;
+}
