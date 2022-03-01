6bd2e2292b49b1b3c1a957037b29083a89961221
Gordon Bateson <gordonbateson@gmail.com>
2022-03-01 12:30:35 +0900

    block_activity_list add zebra stripes to the settings page if 'itemslect' checkboxes are used

--- version.php
+++ version.php
@@ -17,5 +17,5 @@
 $plugin->component = 'block_activity_list';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2022020218;
-$plugin->release   = '2022-02-02 (18)';
+$plugin->version   = 2022030119;
+$plugin->release   = '2022-03-01 (19)';
